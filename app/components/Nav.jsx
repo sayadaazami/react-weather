045d438b275c5b4063c818e0025c985e0b68c56f
sayadaazami <sayadaazami@gmail.com>
2017-09-01 16:38:36 +0430

    add links to nav

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,10 +1,14 @@
 var React = require('react');
+var {Link} = require('react-router');
+// the line above equals to ===> (var Link = require('react-router').Link) uses es6 destructor
+// var user = {name:"sayad", lastname:"aazami"}     =>   var {name} = user    => now name equals to "sayad"
 
 var Nav = React.createClass({
 	render: function(){
 		return (
 			<nav>
-				<h2>Nav Component</h2>
+				<Link to="/"> Get Weather </Link>
+				<Link to="/about"> About  </Link>
 			</nav>
 		);
 	}

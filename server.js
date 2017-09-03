75fabbe9b24f59671e87c54acb92bbffb056e303
sayadaazami <sayadaazami@gmail.com>
2017-09-03 23:43:22 +0430

    add https_middleware, get port from env, add start script

--- server.js
+++ server.js
@@ -3,10 +3,22 @@ var express = require('express');
 //create our app
 var app = express();
 
+//get port from env because of heroku
+const PORT = app.process.env.PORT || 3000;
+
+//add middleware to forwart HTTPS requests to HTTP becuse weather api does not support https
+app.use(function(req, res, next){
+	if(req.headers['x-forwarded-proto'] === 'http'){
+		next();
+	}else{
+		res.redirect('http://' + req.hostname + req.url);
+	}
+});
+
 //define the app root to public
 app.use(express.static('public'));
 
 //run the server (port, closure)
-app.listen(3000, function(){
-  console.log('express run on port 3000');
+app.listen(PORT, function(){
+  console.log('express run on port ' + PORT);
 });
